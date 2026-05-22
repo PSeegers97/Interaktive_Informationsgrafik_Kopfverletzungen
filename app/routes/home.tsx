163c3fafb94d0bbbb8f9baafb05388d1f4d2814e
PSeegers97 <p.seegers87@gmail.com>
2026-05-22 11:53:36 +0200

    p5 integration

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { P5Canvas } from "@p5-wrapper/react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,6 +9,24 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
+
+
+function sketch(p5) {
+  p5.setup = () => p5.createCanvas(600, 400, p5.WEBGL);
+
+  p5.draw = () => {
+    p5.background(250);
+    p5.normalMaterial();
+    p5.push();
+    p5.rotateZ(p5.frameCount * 0.01);
+    p5.rotateX(p5.frameCount * 0.01);
+    p5.rotateY(p5.frameCount * 0.01);
+    p5.plane(100);
+    p5.pop();
+  };
+}
+
+
 export default function Home() {
-  return <Welcome />;
+  return <P5Canvas sketch={sketch} />;
 }
