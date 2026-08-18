33663cb28328530487c76af5da5161657f432162
PSeegers97 <p.seegers87@gmail.com>
2026-08-18 10:42:02 +0200

    changed styling

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,8 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Welcome } from "../components/welcome";
 import { P5Canvas } from "@p5-wrapper/react";
+import { useState, useEffect } from "react";
+import head from '/public/assets/illustration/Kopf.svg';
 
 
 
@@ -15,7 +17,8 @@ export function meta({}: Route.MetaArgs) {
 
 // Hiermit kann ich jeder einzelnen Seite einen neuen p5 Sketch ausführen
 function sketch(p5) {
-  p5.setup = () => p5.createCanvas(p5.displayWidth, p5.displayHeight, p5.WEBGL);
+  p5.setup = () => {
+    p5.createCanvas(p5.displayWidth, p5.displayHeight, p5.WEBGL); }
 
   p5.draw = () => {
     p5.background(250);
@@ -29,12 +32,24 @@ function sketch(p5) {
   };
 }
 
+function getFile(){
+  let result;
+  fetch(head).then(r => result = r ).then(data => console.log(data));
+}
+
+
+  
+
+
+
 
 export default function Home() {
+  
 /*   return <P5Canvas sketch={sketch}  />; */
 return (
-  <a href="Haftungsausschluss" class="w-screen h-screen flex flex-col items-center justify-center">
+  <a href="Schaedel-Hirn-Trauma" class="w-screen h-screen flex flex-col items-center justify-center">
     <h1 class="font-sans font-bold text-headline">Schädel-Hirn-Trauma</h1>
+
   </a>
 )
 }
